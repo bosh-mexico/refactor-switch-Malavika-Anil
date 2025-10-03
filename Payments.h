f4327299064425814f9e73c7681d8a89a53d5b1e
Malavika-Anil <malavikaanil9891@gmail.com>
2025-10-03 09:34:08 +0530

    Update Payments.h

--- Payments.h
+++ Payments.h
@@ -4,19 +4,16 @@
 #include "PaymentProcessor.h"
 #include <iostream>
 
-// PayPal
 class PayPalPayment : public PaymentProcessor {
 public:
     void process(double amount) const override;
 };
 
-// GooglePay
 class GooglePayPayment : public PaymentProcessor {
 public:
     void process(double amount) const override;
 };
 
-// CreditCard
 class CreditCardPayment : public PaymentProcessor {
 public:
     void process(double amount) const override;
